a975a89d1dc5681a0c5a72771b5f05e4192d631b
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-12-17 15:21:26 +0300

    Scripted auto-commit on change (2020-12-17 15:21:26) by gitwatch.sh

--- src/main/java/ru.Ivan/Server.java
+++ src/main/java/ru.Ivan/Server.java
@@ -1,11 +1,32 @@
 package ru.Ivan;
 
+import org.zeromq.SocketType;
 import org.zeromq.ZContext;
+import org.zeromq.ZMQ.*;
 
 public class Server {
+    final private static String CLIENT_SERVER = "tcp://localhost:7000";
+    final private static String SERVER = "tcp://localhost:7777";
+
+    private static Socket clientSocket;
+    private static Socket serverSocket;
+    private static Poller poller;
 
     public static void main(String[] args) {
-        ZContext
+        ZContext cont = new ZContext(1);
+        clientSocket = cont.createSocket(SocketType.ROUTER);
+        clientSocket.bind(CLIENT_SERVER);
+        serverSocket = cont.createSocket(SocketType.ROUTER);
+        serverSocket.bind(SERVER);
+        poller = cont.createPoller(2);
+        poller.register(clientSocket, Poller.POLLIN);
+        poller.register(serverSocket, Poller.POLLIN);
+
+        long time = System.currentTimeMillis();
+
+
+
+
     }
 
 }
