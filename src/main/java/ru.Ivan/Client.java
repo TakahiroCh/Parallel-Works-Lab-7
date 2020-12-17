105c086edadc45214e3689956f2d81ba090c8d97
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-12-17 18:12:56 +0300

    Scripted auto-commit on change (2020-12-17 18:12:56) by gitwatch.sh

--- src/main/java/ru.Ivan/Client.java
+++ src/main/java/ru.Ivan/Client.java
@@ -1,8 +1,18 @@
 package ru.Ivan;
 
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ.*;
+
 public class Client {
+    final private static int TIMEOUT = 7000;
+    final private static String CLIENT_SERVER = "tcp://localhost:7000";
 
     public static void main(String[] args) {
-        
+        ZContext cont = new ZContext(1);
+        Socket client = cont.createSocket(SocketType.REQ);
+        client.setReceiveTimeOut(TIMEOUT);
+        client.connect(CLIENT_SERVER);
+
     }
 }
