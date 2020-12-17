b3219f053e19596073e463e71a154ede9396602f
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-12-17 15:58:08 +0300

    Scripted auto-commit on change (2020-12-17 15:58:08) by gitwatch.sh

--- src/main/java/ru.Ivan/Cache.java
+++ src/main/java/ru.Ivan/Cache.java
@@ -1,2 +1,14 @@
-package ru.Ivan;public class Cache {
+package ru.Ivan;
+
+public class Cache {
+    private long time;
+    private long start;
+    private long finish;
+    private String id;
+
+    public Cache(long start, long end, String id) {
+        this.start = start;
+        this.finish = finish;
+        
+    }
 }
